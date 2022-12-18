49d237b9a694894a4988ef32316f202f1f0e10a6
Matt McKellar-Spence <matt+git@mattms.com>
2022-12-18 20:19:29 +1100

    Add fable-library project template

---
+++ project-templates/fable-library/Library.fs
@@ -0,0 +1,3 @@
+module MattMS.MyLibrary
+
+let getHello name = String.concat " " ["Hello"; name]
---
+++ project-templates/fable-library/build.ps1
@@ -0,0 +1,1 @@
+fable --run esbuild Library.fs.js --bundle --format=iife --global-name=MyLibrary --outfile=Library.fs.bundle.js
